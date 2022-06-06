2ca5cf1c4caf0e6eecc0c034967b8ccb614bb6d7
Cemre <cemresenyuva@outlook.com>
2022-06-06 12:36:17 +0300

    Node Class

---
+++ bookShop/Node.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bookShop;
+
+/**
+ *
+ * @author Cemre Şenyuva, Enes Pusa
+ */
+
+// LinkedList, Stack ve Queue veri yapilarinda kullanilan Node
+public class Node {
+    Object data = new Object();
+    Node next;
+    Node prev;
+ 
+}
