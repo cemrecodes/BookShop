fc45a8cda083f1b8acdc46f75965717db9241f68
Cemre <cemresenyuva@outlook.com>
2022-06-06 12:35:44 +0300

    Login Interface

---
+++ bookShop/Login.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bookShop;
+
+/**
+ *
+ * @author Cemre Şenyuva, Enes Pusa
+ */
+
+// hem uye hem de admin icin giris bulunacagindan dolayi interface turunde login (giris) olusturuldu
+interface Login {
+    boolean login();
+}
